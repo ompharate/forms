633917923ddb2bf3423547f4bde42d3c5e065fd4
ompharate <ompharate31@gmail.com>
2024-07-26 22:34:17 +0530

    formik installed and tailwindcss

---
+++ utils/config.ts
@@ -0,0 +1,13 @@
+import { Account, Client, Databases } from "appwrite";
+
+// export const DATABASES_ID = "64747cafb8c51754933c"
+
+export const PROJECT_ID = "66a3d52800387f6fcaa2"
+
+
+const client = new Client();
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(PROJECT_ID);
+
+export const account = new Account(client);
